3768d2f685fd0e38380770bccf6539286ff8de86
Yul <kyung9687@gmail.com>
2025-01-21 11:09:56 +0900

    깃연결

---
+++ 04_JAVA/01_variable/src/common/PrintEx.java
@@ -0,0 +1,23 @@
+package common;
+
+public class PrintEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * System.out.print();
+		 * 출력 내용을 콘솔에 출력 
+		 * 출력완료후 줄이 바뀌지 않음
+		 *
+		 * System.out.println(); 
+		 * 출력 완료후 줄이 바뀜
+		 * 
+		 * System.out.printf(문자열 템플릿(패턴),패턴에 대입될값/변수);
+		 * 
+		 */
+		
+		String name = "신짱구";
+	}
+
+}
